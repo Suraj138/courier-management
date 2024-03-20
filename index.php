2cbae0509f0bf98078423367ef255d8a29f96044
Suraj Kumar <ksuraj138@gmail.com>
2024-03-20 21:57:03 +0530

    updated footer

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="https://github.com/Suraj138">SURAJ KUMAR</a>.</strong>
+    <strong>Copyright &copy; 2024 <a href="https://github.com/Suraj138">SURAJ KUMAR</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b><?php echo $_SESSION['system']['name'] ?></b>
